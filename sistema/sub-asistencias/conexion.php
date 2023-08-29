0b3babc40760cdc3a3728519767ed38ad965b4ba
Ale Ofi <air5795@gmail.com>
2023-08-29 15:04:41 -0400

    conexion asistencias

--- sistema/sub-asistencias/conexion.php
+++ sistema/sub-asistencias/conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-    $usuario = "root";
-    $password = "";
-    $conexion = new PDO("mysql:host=localhost:3316;dbname=naxsan", $usuario, $password);
+    $usuario = "airsoftb_naxsan";
+    $password = "71811452Ale*";
+    $conexion = new PDO("mysql:host=localhost:3316;dbname=airsoftb_naxsan", $usuario, $password);
