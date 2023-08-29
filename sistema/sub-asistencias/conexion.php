82329306efe3383c78fbc617ccd7def58957d7fe
Ale Ofi <air5795@gmail.com>
2023-08-29 15:06:30 -0400

    d

--- sistema/sub-asistencias/conexion.php
+++ sistema/sub-asistencias/conexion.php
@@ -2,4 +2,4 @@
 
     $usuario = "airsoftb_naxsan";
     $password = "71811452Ale*";
-    $conexion = new PDO("mysql:host=localhost:3316;dbname=airsoftb_naxsan", $usuario, $password);
+    $conexion = new PDO("mysql:host=localhost;dbname=airsoftb_naxsan", $usuario, $password);
